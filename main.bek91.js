5e0c2b593c66057ec339f746c80adc1403248523
Bek <bek.akhmedov@gmail.com>
2025-04-18 16:54:36 -0400

    first commit

---
+++ main.bek91.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Bubble Editor Tweaks
+// @namespace    https://yourusername.github.io
+// @version      0.1
+// @description  Override Bubble.io editor styles (modularized)
+// @match        https://*.bubble.io/page*
+// @grant        GM_addStyle
+// @updateURL    https://yourusername.github.io/bubble-editor-tweaks/main.user.js
+// @downloadURL  https://yourusername.github.io/bubble-editor-tweaks/main.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+  
+    const cssURL = 'https://yourusername.github.io/bubble-editor-tweaks/styles.css';
+  
+    fetch(cssURL)
+      .then((res) => res.text())
+      .then((css) => GM_addStyle(css))
+      .catch((err) => console.error('[Bubble Tweaks] Failed to load CSS:', err));
+  })();
+  
